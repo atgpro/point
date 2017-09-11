9480ebc55d9d2ab0540f6119a1cfe169eaf7f864
Your Name <you@example.com>
2017-09-11 15:03:47 +0500

    аленины правки - все

--- tur-panels.php
+++ tur-panels.php
@@ -367,15 +367,15 @@
               <button class="close-me"></button>
               <div class="header">
                 <div class="row">
-                  <div class="col-sm-6">
+                  <div class="col-xs-6">
                     <div class="cities">
                       <div class="city">Москва<br>от <b>27 311 <span class="thin">Р</span></b></div>
                       <button class="city add">Добавить город вылета</button>
                     </div>
                   </div>
-                  <div class="col-sm-6">
+                  <div class="col-xs-6">
                     <div class="group-by">
-                      <div class="no-group-by">
+                      <div class="no-group-by active">
                         Без группировки
                       </div>
                       <button class="control"></button>
@@ -386,10 +386,107 @@
                   </div>
                 </div>
               </div>
+              
+              <div class="body"> 
+                
+                <div class="tours-table">
+                  <div class="tr">
+                    <div class="th"><div class="icon" style="background-image:url(img/date-white-icon.png)" alt=""></div><span>Дата</span></div>
+                    <div class="th"><div class="icon" style="background-image:url(img/bed-white-icon.png)" alt=""></div><span>Тип номера</span></div>
+                    <div class="th"><div class="icon" style="background-image:url(img/food-white-icon.png)" alt=""></div><span>Питание</span></div>
+                    <div class="th"><div class="icon" style="background-image:url(img/car-white-icon.png)" alt=""></div><span>Экскурсии</span></div>
+                    <div class="th"><div class="icon price" style="background-image:url(img/price-white-icon.png)" alt=""></div><span>Цена тура<br>ТО</span></div>
+                    <div class="th"><span>Визы,<br>документы,<br>срок сдачи</span></div>
+                    <div class="th"><div class="icon price" style="background-image:url(img/price-white-icon.png)" alt=""></div><span>Полная<br></span>стоимость
+                    </div>
+                    <div class="th"><div class="icon price" style="background-image:url(img/ship-white-icon.png)" alt=""></div><span>Туроператор</span>
+                      <button class="open-last-column">Развернуть</button>
+                    </div>
+                    <div class="th last-column"><div class="icon" style="background-image:url(img/calc-white-icon.png)" alt=""></div><span>Расчеты<br>с ЦБТ</span>
+                      <button class="hide-last-column">Свернуть</button>
+                    </div>
+                  </div>
 
-              <div class="body">
+                  <div class="tr">
+                    <div class="filter">
+                      <a href="#"><span>По алфавиту</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По алфавиту</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По алфавиту</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По алфавиту</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По порядку</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По порядку</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter">
+                      <a href="#"><span>По порядку</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter last-column">
+                      <a href="#"><span>По порядку</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                    <div class="filter last-column">
+                      <a href="#"><span>По порядку</span><div class="bars"><i></i><i></i><i></i></div></a>
+                    </div>
+                  </div>
+                  
+                  <div class="table-content">
+                    <div class="tr">
+                      <div class="td"><span class="important">12.08.17<br>-19.08.17</span><span class="muted">7 ночей</span> <a href="#" class="flies">Авиаперелеты</a></div>
+                      <div class="td"><span class="important">DBL-standart</span></div>
+                      <div class="td"><span class="important">BB (Завтрак)</span></div>
+                      <div class="td"><div class="icon" style="background-image:url(img/check-icon.png)" alt=""></div><a href="#">Подробнее</a></div>
+                      <div class="td"><span class="important">17 872 Р</span> <span class="muted">
+                      Цена без<br>доплат</span></div>
+                      <div class="td"><span class="important">9 439Р</span><a href="#">Документы</a><span class="muted">до 22.07.2017</span></div>
+                      <div class="td"><span class="important"><b>27 311</b> Р</span><span class="muted">В кредит:</span><a href="#">от 4 786 Р/мес</a></div>
+                      
+                      <div class="td"><span class="important"><img  class="op-logo" src="img/operator-logo.png" alt=""><span class="muted">1 USD = 61,14</span> <span class="muted">1 EURO = 71,75</span> </div>
+
+                      <div class="td last-column"><span class="muted">Комиссия*<br>(до 5.6%) <br>до 1 529 </span><span class="muted">К оплате ТО*: <br>до 25 782 </span></div>
+                      <a href="#" class="add-to-basket"><span>Заказать тур</span><img src="img/basket-white-icon.png" alt=""></a>
+                    </div>
+                    <div class="tr">
+                      <div class="td"><span class="important">12.08.17<br>-19.08.17</span><span class="muted">7 ночей</span> <a href="#" class="flies">Авиаперелеты</a></div>
+                      <div class="td"><span class="important">DBL-standart</span></div>
+                      <div class="td"><span class="important">BB (Завтрак)</span></div>
+                      <div class="td"><div class="icon" style="background-image:url(img/check-icon.png)" alt=""></div><a href="#">Подробнее</a></div>
+                      <div class="td"><span class="important">17 872 Р</span> <span class="muted">
+                      Цена без<br>доплат</span></div>
+                      <div class="td"><span class="important">9 439Р</span><a href="#">Документы</a><span class="muted">до 22.07.2017</span></div>
+                      <div class="td"><span class="important"><b>27 311</b> Р</span><span class="muted">В кредит:</span><a href="#">от 4 786 Р/мес</a></div>
+                      <div class="td"><span class="important"><img  class="op-logo" src="img/operator-logo.png" alt=""><span class="muted">1 USD = 61,14</span> <span class="muted">1 EURO = 71,75</span> </div>
+                      <div class="td last-column"><span class="muted">Комиссия*<br>(до 5.6%) <br>до 1 529 </span><span class="muted">К оплате ТО*: <br>до 25 782 </span></div>
+                      <a href="#" class="add-to-basket"><span>Заказать тур</span><img src="img/basket-white-icon.png" alt=""></a>
+                    </div>
+                    <div class="tr">
+                      <div class="td"><span class="important">12.08.17<br>-19.08.17</span><span class="muted">7 ночей</span> <a href="#" class="flies">Авиаперелеты</a></div>
+                      <div class="td"><span class="important">DBL-standart</span></div>
+                      <div class="td"><span class="important">BB (Завтрак)</span></div>
+                      <div class="td"><div class="icon" style="background-image:url(img/check-icon.png)" alt=""></div><a href="#">Подробнее</a></div>
+                      <div class="td"><span class="important">17 872 Р</span> <span class="muted">
+                      Цена без<br>доплат</span></div>
+                      <div class="td"><span class="important">9 439Р</span><a href="#">Документы</a><span class="muted">до 22.07.2017</span></div>
+                      <div class="td"><span class="important"><b>27 311</b> Р</span><span class="muted">В кредит:</span><a href="#">от 4 786 Р/мес</a></div>
+                      <div class="td"><span class="important"><img  class="op-logo" src="img/operator-logo.png" alt=""><span class="muted">1 USD = 61,14</span> <span class="muted">1 EURO = 71,75</span> </div>
+                      <div class="td last-column"><span class="muted">Комиссия*<br>(до 5.6%) <br>до 1 529 </span><span class="muted">К оплате ТО*: <br>до 25 782 </span></div>
+                      <a href="#" class="add-to-basket"><span>Заказать тур</span><img src="img/basket-white-icon.png" alt=""></a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+
+              <div class="body grouped">
                 <div class="operator">
-                  <img src="img/operator-logo.png" alt="" class="logo">
+                  <img  class="op-logo" src="img/operator-logo.png" alt="" class="logo">
                   <div class="name">Библиоглобус</div>
                   <div class="currencies">
                     <div class="item">
